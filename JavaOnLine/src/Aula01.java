d3feb86eebf49f8483d4f2a3105592bb81dba600
AlexsandroMatias2017 <matiasalexsandro@gmail.com>
2019-07-01 17:33:53 -0300

    Primeira aula de introdução (motivação da linguagem Java)

--- JavaOnLine/src/Aula01.java
+++ JavaOnLine/src/Aula01.java
@@ -1,4 +1,23 @@
-
-public class Aula01 {
-
+//Aula 01 - Como Java funciona 
+public class Aula01
+{
+	// O problema em programar para cada sistema operacional era por que o
+	// processo de compilação é feito para tal hardware específico.
+	// Esse processo é chamado de linguagem de máquina.
+	// Então executar programa compilado num determinado hardware, não garantia
+	// este
+	// que seria executado em outro hardware.
+	// Para resolver esse problema, a linguagem Java adicionou uma nova camada.
+	//
+	// Essa camada adicionada em cima dos sistemas operacionais chamada de JVM -
+	// Java Virtual Machine que serve como interpretador da linguaguem. Então
+	// quando compilado um código fonte em Java será compreendido pela JVM.
+	// Então o mesmo código, poderá ser executado em qualquer sistema
+	// operacional que possua a JVM.
+	// A linguagem compilada do Java é chamada de Bytecode. Todas as vezes que é
+	// criado algum programa em Java, este terá a extensão .java.
+	// Já para compilarmos esse programa ele se tornará .class.
+	//
+	// teste.java -> JVM -> teste.class
+	// WORA - write once run anywhere
 }
