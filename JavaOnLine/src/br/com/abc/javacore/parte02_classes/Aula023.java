006b3d829321d17b053bf5a740a1b578939a1d8a
almei <AlexsandroMatias2017>
2020-01-08 09:51:38 -0300

    Inicializado pacotes em Java

---
+++ JavaOnLine/src/br/com/abc/javacore/parte02_classes/Aula023.java
@@ -0,0 +1,11 @@
+package br.com.abc.javacore.parte02_classes;
+
+//forma de organização de diretórios para melhor encontrar nossas 
+//classes e representa a primeira linha de uma classe, senão dará um erro compilação
+public class Aula023
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Aula 23 - Pacotes em Java");
+	}
+}
