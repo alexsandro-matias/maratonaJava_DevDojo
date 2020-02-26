c6d10c2228574814371be9a0843f70728dcf4d0a
AlexsandroMatias2017 <matiasalexsandro@gmail.com>
2020-02-26 14:23:08 -0300

    Aula 100

--- JavaOnLine/src/br/com/abc/javacore/parte20_data/test/DatasTest.java
+++ JavaOnLine/src/br/com/abc/javacore/parte20_data/test/DatasTest.java
@@ -6,43 +6,6 @@ public class DatasTest
 {
 	public static void main(String[] args)
 	{
-		Calendar calendar = new Calendar()
-		{
-			@Override public void roll(int field, boolean up)
-			{
-			}
-			
-			@Override public int getMinimum(int field)
-			{
-				return 0;
-			}
-			
-			@Override public int getMaximum(int field)
-			{
-				return 0;
-			}
-			
-			@Override public int getLeastMaximum(int field)
-			{
-				return 0;
-			}
-			
-			@Override public int getGreatestMinimum(int field)
-			{
-				return 0;
-			}
-			
-			@Override protected void computeTime()
-			{
-			}
-			
-			@Override protected void computeFields()
-			{
-			}
-			
-			@Override public void add(int field, int amount)
-			{
-			}
-		};
+		Calendar calendar = new Calendar();
 	}
 }
