755fce7c20d2d3a1dfde0cd1de240017e85d14b3
PB7P <PB7P@MI00273033.petrobras.biz>
2021-04-10 05:21:20 -0300

    Collections

--- JavaOnLine/src/br/com/abc/javacore/parte00_colecoes/classes/SortProdutoTeste.java
+++ JavaOnLine/src/br/com/abc/javacore/parte00_colecoes/classes/SortProdutoTeste.java
@@ -3,7 +3,6 @@ package br.com.abc.javacore.parte00_colecoes.classes;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
