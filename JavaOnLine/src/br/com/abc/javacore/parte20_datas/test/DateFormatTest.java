998add7aa93898119928707dcab958f9ff7d46bd
alexsandro-matias <matiasalexsandro@gmail.com>
2023-03-12 22:25:03 -0300

    Tentando aprender as classes de Datas em Java

--- JavaOnLine/src/br/com/abc/javacore/parte20_datas/test/DateFormatTest.java
+++ JavaOnLine/src/br/com/abc/javacore/parte20_datas/test/DateFormatTest.java
@@ -4,10 +4,8 @@ package br.com.abc.javacore.parte20_datas.test;
 import java.text.DateFormat;
 import java.util.Calendar;
 
-public class DateFormatTest
-{
-	public static void main(String[] args)
-	{
+public class DateFormatTest {
+	public static void main(String[] args) {
 		Calendar c = Calendar.getInstance();
 		DateFormat[] dataFormatadas = new DateFormat[6];
 		dataFormatadas[0] = DateFormat.getInstance();
@@ -16,8 +14,7 @@ public class DateFormatTest
 		dataFormatadas[3] = DateFormat.getDateInstance(DateFormat.MEDIUM);
 		dataFormatadas[4] = DateFormat.getDateInstance(DateFormat.LONG);
 		dataFormatadas[5] = DateFormat.getDateInstance(DateFormat.FULL);
-		for (DateFormat dateFormat : dataFormatadas)
-		{
+		for (DateFormat dateFormat : dataFormatadas) {
 			System.out.println(dateFormat.format(c.getTime()));
 		}
 	}
--- JavaOnLine/src/br/com/abc/javacore/parte20_datas/test/ManipulacaoDeDatas.java
+++ JavaOnLine/src/br/com/abc/javacore/parte20_datas/test/ManipulacaoDeDatas.java
@@ -4,37 +4,56 @@ package br.com.abc.javacore.parte20_datas.test;
 
 import java.util.Calendar;
 import java.util.Date;
+//Desde a versão 1.0 java existe uma classe 
+//chamada Date contida no pacote de utilitários que é responsável por datas. Porém ela não é muito bom com internacionalização.
+//Para resolver essa situação, na versão 1.1 foi implementada a classe Calendar. Já a partir do Java 8, 
+//também foi criada um novo pacote chamado de Time.
+//Na generalidade, os métodos de Date estão obsoletos (deprecated)
 
 public class ManipulacaoDeDatas
 {
 	public static void main(String[] args)
 	{
-		Date data = new Date(1_000_000_000_000L); // em milissegundo
-		System.out.println(data);
-		//
-		Date dataAtual = new Date();
-		System.out.println(dataAtual);
-		//
-		// Uma classe muito complicada com manipulação dos dados com vários métodos
-		// depreciados. Para isso foi utilizada a classe Abstrata Calendar
-		Calendar c = Calendar.getInstance();
-		System.out.println(c);
-		c.setTime(dataAtual);
-		if (Calendar.SUNDAY == c.getFirstDayOfWeek())
-		{
-			System.out.println("Domingo é o primeiro dia da semana");
-		}
-		System.out.println(Calendar.DAY_OF_MONTH);
-		System.out.println(Calendar.DAY_OF_WEEK);
-		System.out.println(Calendar.DAY_OF_WEEK_IN_MONTH);
-		System.out.println(Calendar.DAY_OF_YEAR);
+		// Tempo em milissegundo
+		// Date data = new Date(1_000_000_000_000L);
+		// System.out.println(data);
+		// Date dataAtual = new Date();
+		// System.out.println(dataAtual);
+		// //
+		// // Uma classe muito complicada com manipulação dos dados com vários
+		// // métodos
+		// // depreciados. Para isso foi utilizada a classe Abstrata Calendar
+		// Calendar c = Calendar.getInstance();
+		// c.setTime(dataAtual);
+		// if (Calendar.SUNDAY == c.getFirstDayOfWeek())
+		// {
+		// System.out.println("Domingo é o primeiro dia da semana");
+		// }
+		// System.out.println(c.get(Calendar.DAY_OF_MONTH));
+		// System.out.println(c.get(Calendar.DAY_OF_WEEK));
+		// System.out.println(c.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+		// System.out.println(c.get(Calendar.DAY_OF_YEAR));
 		// Para adicionar uma hora
-		c.add(Calendar.HOUR, 2);
-		c.add(Calendar.MONTH, 2);
-		// Para manter o mesmo ano
-		c.roll(Calendar.MONTH, 9);
-		Date data2 = c.getTime();
-		System.out.println(data2);
-		//
+		// c.add(Calendar.HOUR, 2);
+		// c.add(Calendar.MONTH, 2);
+		// // Para manter o mesmo ano
+		// c.roll(Calendar.MONTH, 9);
+		// Date data2 = c.getTime();
+		// System.out.println(data2);
+		Date data = new Date();
+		Calendar momentoAtual = Calendar.getInstance();
+		momentoAtual.setTime(data);
+		// Adicionando 4 horas pra que o dia passe
+		// para amanhã
+		momentoAtual.add(Calendar.DAY_OF_WEEK, 6);
+		int hoje = momentoAtual.get(Calendar.DAY_OF_WEEK);
+		if ((hoje == 1) || (hoje == 7))
+		{
+			System.out.println("Hoje é final de semana");
+		}
+		else
+		{
+			System.out.println("Hoje é meio de semana");
+		}
 	}
 }
