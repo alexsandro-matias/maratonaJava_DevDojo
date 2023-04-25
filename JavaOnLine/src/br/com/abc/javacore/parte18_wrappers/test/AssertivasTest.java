43cb85d1604485dc00a7388d59dd113de232a3e3
alexsandro-matias <matiasalexsandro@gmail.com>
2023-04-25 20:31:00 -0300

    revisando conceitos

--- JavaOnLine/src/br/com/abc/javacore/parte18_wrappers/test/AssertivasTest.java
+++ JavaOnLine/src/br/com/abc/javacore/parte18_wrappers/test/AssertivasTest.java
@@ -1,17 +1,15 @@
 //Aula 77 - Assertivas
 package br.com.abc.javacore.parte18_wrappers.test;
 
-public class AssertivasTest
-{
+public class AssertivasTest {
 	// Assertivas é para que valores sejam verificados e caso esse valor não
 	// esperado será lançado um erro. Um exemplo seria um valor negativo para
 	// salário. O assert sempre vai garantir que o valor esperado seja utilizado.
 	// Ele é ignorado, para que funcione é necessário que seja ativado.
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		calculaSalario(-2000);
 	}
-	
+
 	private static void calculaSalario(double salario)
 	// esse método deve ser privado para que somente a equipe possa
 	// escolher tais valores.
@@ -30,22 +28,18 @@ public class AssertivasTest
 	}
 	//
 	// Caso queira fazer tratamento
-	
-	public static void calcularSalario(double salario)
-	{
-		if (salario < 0)
-		{
+
+	public static void calcularSalario(double salario) {
+		if (salario < 0) {
 			throw new IllegalArgumentException();
 		}
 	}
-	
-	public static void diasDaSemana(int dia)
-	{
-		switch (dia)
-		{
-			case 1:
-				break;
-			default:
+
+	public static void diasDaSemana(int dia) {
+		switch (dia) {
+		case 1:
+			break;
+		default:
 			assert false;
 			// System.out.println("Dia um inválido");
 			// break;
